cb29435b85ed94c2d1c85c7a1358073f5ebe2038
dylcruz <dylan@dylancruz.me>
2021-08-31 12:58:19 -0400

    fixed and shortened replacement logic

--- chapter9/madlibs/madLibs.py
+++ chapter9/madlibs/madLibs.py
@@ -1,26 +1,18 @@
-from pathlib import Path
+import re
 
 path = '/Users/dylan/python/Automate_The_Boring_Stuff/chapter9/madlibs/testSentence'
-
 sentenceFile = open(f'{path}.txt')
-sentenceSplit = sentenceFile.read().split()
+sentence = sentenceFile.read()
+sentenceFile.close()
 
-for i in range(len(sentenceSplit)):
-    if sentenceSplit[i] == 'ADJECTIVE':
-        print('Please enter an adjective: ', end = '')
-        sentenceSplit[i] = input()
-    elif sentenceSplit[i] == 'NOUN':
-        print('Please enter a noun: ', end = '')
-        sentenceSplit[i] = input()
-    elif sentenceSplit[i] == 'VERB':
-        print('Please enter an verb: ', end = '')
-        sentenceSplit[i] = input()
-    else:
-        continue
+findReplacement = re.compile(r'(NOUN|VERB|ADJECTIVE)')
 
-sentence = ' '.join(sentenceSplit)
+for match in findReplacement.findall(sentence):   
+    sentence = findReplacement.sub(input(f'Please enter a(n) {match}: '), sentence, count = 1)
+
 print(sentence)
 
 newPath = f'{path}_complete.txt'
-completeFile = open(newPath, 'w')
-completeFile.write(sentence)
+newSentenceFile = open(f'{path}_complete.txt')
+newSentenceFile.write(sentence)
+newSentenceFile.close()
