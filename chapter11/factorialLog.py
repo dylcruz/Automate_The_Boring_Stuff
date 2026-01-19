60284305ddfa79f98236168c820e41ca797fbfc7
Dylan Cruz <dylan@dylancruz.me>
2026-01-19 13:25:51 -0800

    finished chapter 11

---
+++ chapter11/factorialLog.py
@@ -0,0 +1,23 @@
+import logging
+
+logging.basicConfig(
+    filename="myProgramLog.txt",
+    level=logging.DEBUG,
+    format=" %(asctime)s -  %(levelname)s -  %(message)s",
+)
+logging.disable(logging.CRITICAL)
+logging.debug("Start of program")
+
+
+def factorial(n):
+    logging.debug(f"Start of factorial ({str(n)})")
+    total = 1
+    for i in range(1, n + 1):
+        total *= i
+        logging.debug(f"i is {str(i)}, total is {str(total)}")
+    logging.debug(f"End of factorial {str(n)}")
+    return total
+
+
+print(factorial(5))
+logging.debug("End of program")
